2c1ceba7eed7594648fb37a3af2c748bcca8511f
«Egor <egoled8@gmail.com>
2022-06-19 19:07:16 +0300

    первая задача от 13.06

---
+++ Exercise_1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число
+// и проверяет, является ли оно палиндромом
+
+
+string Prompt (string message)
+{
+    System.Console.WriteLine(message);
+    string num = Console.ReadLine();
+    return num;
+}
+string num = Prompt("Введите пятизначное число ");
+
+if (num.Length != 5)
+{
+    Console.WriteLine($"{num} - данное число не пятизначное");
+}
+else
+{
+    if (num[0] == num[4] && num[1]  == num[3] ) 
+    {
+        Console.WriteLine($"{num} - палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{num} не палиндром");
+    }
+}
